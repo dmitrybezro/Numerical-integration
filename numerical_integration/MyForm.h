2f1bdf261f8f3faa95783b59957e3bac92d0a5da
dmitrybezro <d.bezrodnow@gmail.com>
2021-05-17 18:08:19 +0300

    Реализована 2-я часть задачи. На основе метода адаптивной квадратуры строится график функции g(x), а так же график трудоемкости вычислений

--- numerical_integration/MyForm.h
+++ numerical_integration/MyForm.h
@@ -1,5 +1,11 @@
 ﻿#pragma once
+#define _USE_MATH_DEFINES
 #include<cmath>
+#include<vector>
+double alpha;
+std::vector<double> A(14);
+std::vector<double> B(14);
+
 namespace numericalintegration {
 
 	using namespace System;
@@ -59,6 +65,18 @@ namespace numericalintegration {
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Label^ label11;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::ComboBox^ comboBox3;
+	private: ZedGraph::ZedGraphControl^ zedGraphControl2;
+	private: System::Windows::Forms::TabControl^ tabControl2;
+	private: System::Windows::Forms::TabPage^ tabPage3;
+	private: System::Windows::Forms::TabPage^ tabPage4;
+	private: ZedGraph::ZedGraphControl^ zedGraphControl3;
 	private: System::ComponentModel::IContainer^ components;
 
 
@@ -66,6 +84,7 @@ namespace numericalintegration {
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
+		
 
 
 #pragma region Windows Form Designer generated code
@@ -94,18 +113,34 @@ namespace numericalintegration {
 			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->zedGraphControl1 = (gcnew ZedGraph::ZedGraphControl());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->tabControl2 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
+			this->zedGraphControl2 = (gcnew ZedGraph::ZedGraphControl());
+			this->tabPage4 = (gcnew System::Windows::Forms::TabPage());
+			this->zedGraphControl3 = (gcnew ZedGraph::ZedGraphControl());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->label11 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
+			this->tabPage2->SuspendLayout();
+			this->tabControl2->SuspendLayout();
+			this->tabPage3->SuspendLayout();
+			this->tabPage4->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// tabControl1
 			// 
 			this->tabControl1->Controls->Add(this->tabPage1);
 			this->tabControl1->Controls->Add(this->tabPage2);
-			this->tabControl1->Location = System::Drawing::Point(2, 1);
+			this->tabControl1->Location = System::Drawing::Point(2, 3);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(1239, 654);
+			this->tabControl1->Size = System::Drawing::Size(1239, 652);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// tabPage1
@@ -128,16 +163,18 @@ namespace numericalintegration {
 			this->tabPage1->Location = System::Drawing::Point(4, 29);
 			this->tabPage1->Name = L"tabPage1";
 			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage1->Size = System::Drawing::Size(1231, 621);
+			this->tabPage1->Size = System::Drawing::Size(1231, 619);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"Задача 1";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
 			// button1
 			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->button1->Location = System::Drawing::Point(107, 299);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(129, 45);
+			this->button1->Size = System::Drawing::Size(139, 45);
 			this->button1->TabIndex = 16;
 			this->button1->Text = L"Вычислить";
 			this->button1->UseVisualStyleBackColor = true;
@@ -290,18 +327,160 @@ namespace numericalintegration {
 			this->zedGraphControl1->ScrollMinX = 0;
 			this->zedGraphControl1->ScrollMinY = 0;
 			this->zedGraphControl1->ScrollMinY2 = 0;
-			this->zedGraphControl1->Size = System::Drawing::Size(666, 460);
+			this->zedGraphControl1->Size = System::Drawing::Size(675, 482);
 			this->zedGraphControl1->TabIndex = 0;
 			// 
 			// tabPage2
 			// 
+			this->tabPage2->Controls->Add(this->tabControl2);
+			this->tabPage2->Controls->Add(this->button2);
+			this->tabPage2->Controls->Add(this->textBox5);
+			this->tabPage2->Controls->Add(this->label11);
+			this->tabPage2->Controls->Add(this->textBox4);
+			this->tabPage2->Controls->Add(this->label4);
+			this->tabPage2->Controls->Add(this->label10);
+			this->tabPage2->Controls->Add(this->comboBox3);
 			this->tabPage2->Location = System::Drawing::Point(4, 29);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(1231, 621);
+			this->tabPage2->Size = System::Drawing::Size(1231, 619);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Задача 2";
 			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// tabControl2
+			// 
+			this->tabControl2->Controls->Add(this->tabPage3);
+			this->tabControl2->Controls->Add(this->tabPage4);
+			this->tabControl2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->tabControl2->Location = System::Drawing::Point(457, 27);
+			this->tabControl2->Name = L"tabControl2";
+			this->tabControl2->SelectedIndex = 0;
+			this->tabControl2->Size = System::Drawing::Size(675, 491);
+			this->tabControl2->TabIndex = 24;
+			// 
+			// tabPage3
+			// 
+			this->tabPage3->Controls->Add(this->zedGraphControl2);
+			this->tabPage3->Location = System::Drawing::Point(4, 29);
+			this->tabPage3->Name = L"tabPage3";
+			this->tabPage3->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage3->Size = System::Drawing::Size(667, 458);
+			this->tabPage3->TabIndex = 0;
+			this->tabPage3->Text = L"График функции g(x)";
+			this->tabPage3->UseVisualStyleBackColor = true;
+			// 
+			// zedGraphControl2
+			// 
+			this->zedGraphControl2->Location = System::Drawing::Point(0, 1);
+			this->zedGraphControl2->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->zedGraphControl2->Name = L"zedGraphControl2";
+			this->zedGraphControl2->ScrollGrace = 0;
+			this->zedGraphControl2->ScrollMaxX = 0;
+			this->zedGraphControl2->ScrollMaxY = 0;
+			this->zedGraphControl2->ScrollMaxY2 = 0;
+			this->zedGraphControl2->ScrollMinX = 0;
+			this->zedGraphControl2->ScrollMinY = 0;
+			this->zedGraphControl2->ScrollMinY2 = 0;
+			this->zedGraphControl2->Size = System::Drawing::Size(662, 447);
+			this->zedGraphControl2->TabIndex = 23;
+			// 
+			// tabPage4
+			// 
+			this->tabPage4->Controls->Add(this->zedGraphControl3);
+			this->tabPage4->Location = System::Drawing::Point(4, 29);
+			this->tabPage4->Name = L"tabPage4";
+			this->tabPage4->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage4->Size = System::Drawing::Size(667, 458);
+			this->tabPage4->TabIndex = 1;
+			this->tabPage4->Text = L"График функции трудоемкости T(x)";
+			this->tabPage4->UseVisualStyleBackColor = true;
+			// 
+			// zedGraphControl3
+			// 
+			this->zedGraphControl3->Location = System::Drawing::Point(0, 1);
+			this->zedGraphControl3->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->zedGraphControl3->Name = L"zedGraphControl3";
+			this->zedGraphControl3->ScrollGrace = 0;
+			this->zedGraphControl3->ScrollMaxX = 0;
+			this->zedGraphControl3->ScrollMaxY = 0;
+			this->zedGraphControl3->ScrollMaxY2 = 0;
+			this->zedGraphControl3->ScrollMinX = 0;
+			this->zedGraphControl3->ScrollMinY = 0;
+			this->zedGraphControl3->ScrollMinY2 = 0;
+			this->zedGraphControl3->Size = System::Drawing::Size(662, 447);
+			this->zedGraphControl3->TabIndex = 24;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(82, 220);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(129, 45);
+			this->button2->TabIndex = 22;
+			this->button2->Text = L"Построить";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(194, 146);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(135, 26);
+			this->textBox5->TabIndex = 21;
+			this->textBox5->Text = L"0,01";
+			// 
+			// label11
+			// 
+			this->label11->AutoSize = true;
+			this->label11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label11->Location = System::Drawing::Point(6, 145);
+			this->label11->Name = L"label11";
+			this->label11->Size = System::Drawing::Size(100, 25);
+			this->label11->TabIndex = 20;
+			this->label11->Text = L"Точность";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(194, 85);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(135, 26);
+			this->textBox4->TabIndex = 19;
+			this->textBox4->Text = L"100";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(6, 84);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(172, 25);
+			this->label4->TabIndex = 18;
+			this->label4->Text = L"Число разбиений";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label10->Location = System::Drawing::Point(6, 23);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(74, 25);
+			this->label10->TabIndex = 17;
+			this->label10->Text = L"Метод";
+			// 
+			// comboBox3
+			// 
+			this->comboBox3->FormattingEnabled = true;
+			this->comboBox3->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"прямоугольников", L"трапеций", L"Симпсона" });
+			this->comboBox3->Location = System::Drawing::Point(117, 24);
+			this->comboBox3->Name = L"comboBox3";
+			this->comboBox3->Size = System::Drawing::Size(212, 28);
+			this->comboBox3->TabIndex = 16;
 			// 
 			// MyForm
 			// 
@@ -314,6 +493,11 @@ namespace numericalintegration {
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			this->tabPage1->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
+			this->tabControl2->ResumeLayout(false);
+			this->tabPage3->ResumeLayout(false);
+			this->tabPage4->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
@@ -337,6 +521,7 @@ private:
 		if (number_function == 3) {
 			return sin(_x) * cos(2 * _x) + cos(100 * _x);
 		}
+
 	}
 	double true_value(double a, double b) {
 		if (number_function == 1) {
@@ -488,6 +673,141 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 
 }
 
+private:
+	void Random(double& _alpha, std::vector<double>& A, std::vector<double>& B) {
+		_alpha =  double(rand()) / RAND_MAX;
+
+		for (int i = 0; i < A.size(); i++) {
+			A[i] = double(rand()) / RAND_MAX * (2) - 1;
+		}
+
+		for (int i = 0; i < B.size(); i++) {
+			B[i] = double(rand()) / RAND_MAX * (2) - 1;
+		}
+	}
+	double Adaptive_Quadrature(double a, double b, int N, double eps, double _x, String^ method) {
+		count++;
+		double I = method_integration(a, b, N, _x, method);
+
+		double I1 = method_integration(a, (a + b) / 2, N, _x, method);
+		double I2 = method_integration((a + b) / 2, b, N, _x, method);
+
+		if (abs(I1 + I2 - I) < eps)
+			return I;
+		else {
+			double Integral = Adaptive_Quadrature(a, (a + b) / 2, N, eps / 2, _x, method) +
+				Adaptive_Quadrature((a + b) / 2, b, N, eps / 2, _x, method);
+			return Integral;
+		}
+	}
+
+	double func_G(double _t, double x) {
+		double res = 0;
+		for (int i = 0; i < 14; i++) {
+			res += A[i] * sin(2 * M_PI * i * (alpha - x) * _t) + B[i] * cos(2 * M_PI * i * (alpha - x) * _t);
+		}
+		return res;
+	}
+
+	double method_integration(double a, double b, double N, double x, String^ method) {
+		//  Шаг
+		double h = (b - a) / N;
+
+		double curr_x = a;
+		double I = 0;
+		if (method == "прямоугольников") {
+			for (int i = 0; i < N; i++) {
+				I += func_G((curr_x + (curr_x + h)) / 2, x) * h;
+				curr_x += h;
+			}
+		}
+
+		if (method == "трапеций") {
+			for (int i = 0; i < N; i++) {
+				I += (func_G(curr_x, x) + func_G(curr_x + h, x)) * h / 2;
+				curr_x += h;
+			}
+		}
+
+		if (method == "Симпсона") {
+			for (int i = 0; i < N; i++) {
+				I += (func_G(curr_x, x) + 4 * func_G(curr_x + h / 2, x) + 
+					func_G(curr_x + h, x)) * h / 6;
+				curr_x += h;
+			}
+		}
+
+		return I;
+	}
+	int count = 0;
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	
+	//  Метод расчета интеграла
+	String^ method = Convert::ToString(comboBox3->Text);
+
+	//  Число разбиений
+	int N = Convert::ToInt32(textBox4->Text);
+
+	//  Точность
+	double eps = Convert::ToDouble(textBox5->Text);
+
+	double min = 0;
+	double max = 0;
+
+	int max_T = 0;
+
+	Random(alpha, A, B);
+	GraphPane^ panel = zedGraphControl2->GraphPane;
+	panel->CurveList->Clear();
+	PointPairList^ f1_list = gcnew ZedGraph::PointPairList();
+
+	GraphPane^ panel2 = zedGraphControl3->GraphPane;
+	panel2->CurveList->Clear();
+	PointPairList^ f2_list = gcnew ZedGraph::PointPairList();
+	if (method != "") {
+		//  Интегрировать снова для каждого значения Х
+		for (double x = 0; x <= 1; x = x + 0.1) {
+			count = 0;
+			double value = Adaptive_Quadrature(-M_PI / 2, M_PI / 2, N, eps, x, method);
+			f1_list->Add(x, value);
+			if (value > max) {
+				max = value;
+			}
+			if (value < min) {
+				min = value;
+			}
+			if (count > max_T) {
+				max_T = count;
+			}
+			f2_list->Add(x, count);
+		}
+		LineItem Curve1 = panel->AddCurve("График функции g(x)", f1_list, Color::Blue, SymbolType::None);
+		LineItem Curve2 = panel2->AddCurve("График функции T(x)", f2_list, Color::Green, SymbolType::None);
+
+		// Устанавливаем интересующий нас интервал по оси X
+		panel->XAxis->Scale->Min = -0.5;
+		panel->XAxis->Scale->Max = 1.5;
+
+		panel2->XAxis->Scale->Min = -0.5;
+		panel2->XAxis->Scale->Max = 1.5;
+
+		// Устанавливаем интересующий нас интервал по оси Y
+		panel->YAxis->Scale->Min = min - 0.5;
+		panel->YAxis->Scale->Max = max + 0.5;
+
+		panel2->YAxis->Scale->Min = -0.5;
+		panel2->YAxis->Scale->Max = max_T + 0.5;
+
+		//  Обновляем оси
+		zedGraphControl1->AxisChange();
+		zedGraphControl2->AxisChange();
+
+		// Обновляем график
+		zedGraphControl1->Invalidate();
+		zedGraphControl2->Invalidate();
+	}
+
+}
 	private: void DrawGraph()
 	{
 		GraphPane^ panel = zedGraphControl1->GraphPane;
@@ -499,6 +819,27 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 
 		zedGraphControl1->AxisChange();
 		zedGraphControl1->Invalidate();
+
+		GraphPane^ panel2 = zedGraphControl2->GraphPane;
+
+		panel2->XAxis->Title->Text = "Ось X";
+		panel2->YAxis->Title->Text = "Ось G";
+
+		panel2->Title->Text = "Функция g(x)";
+
+		zedGraphControl2->AxisChange();
+		zedGraphControl2->Invalidate();
+
+
+		GraphPane^ panel3 = zedGraphControl3->GraphPane;
+
+		panel3->XAxis->Title->Text = "Ось X";
+		panel3->YAxis->Title->Text = "Ось глубины рекурсии";
+
+		panel3->Title->Text = "Функция трудоемкости";
+
+		zedGraphControl3->AxisChange();
+		zedGraphControl3->Invalidate();
 
 
 	}
